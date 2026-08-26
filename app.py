c568fce6f405b59582eebe200f2a78da74bd0091
shotaueda <su0522onedirect@gmail.com>
2026-08-26 10:36:46 +0900

    app.py 71行目再修正

--- app.py
+++ app.py
@@ -68,7 +68,7 @@ def get_token_struct() -> bytes:
     )
     token = credential.get_token("https://database.windows.net/.default")
     token_bytes = token.token.encode("utf-16-le")
-    return struct.pack(f" pyodbc.Connection:
+    return struct.pack(f"<i{len(token_bytes)}s",> pyodbc.Connection:
     """Azure SQL への認証済みコネクションを返す"""
     return pyodbc.connect(
         CONNECTION_STRING, attrs_before={1256: get_token_struct()}
